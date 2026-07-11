d9c0ca27389ccaa4e55b181e52f6feeb12ec9e4d
Sajeda Hmeda <sajedahm18@icloud.com>
2026-07-11 16:42:22 +0200

    Add ClassModel header

---
+++ classmodel.h
@@ -0,0 +1,25 @@
+#ifndef CLASSMODEL_H
+#define CLASSMODEL_H
+#include<string>
+#include<vector>
+using namespace std;
+class ClassModel{
+    private:
+    string className;
+    string baseClass;// اسم الكلاس الاب لو في وراثه
+    vector<string> attributes;//لتخزين المتغيرات
+    vector<string> methods; //لتخزين الدوال
+    public:
+    ClassModel(){}
+    ClassModel(string name, string base=" "):className(name),baseClass(base){}
+    (Getters & Setters)
+    void setClassName(const string& name){className = name;}
+    string getClassName()const{return ClassName;}
+    void setBaseClass(const string& base){baseClass = base;}
+    string getBaseClass()const {return baseClass;}
+    void addAttribute(const string& attr){attributes.push_back(attr);}
+    const vector<string>& getAttributes() const{return attributes;}
+    void addMethod(const string& meth){methods.push_back(meth);}
+    const vector<string>& getMethods()const{return methods;}
+};
+#endif
