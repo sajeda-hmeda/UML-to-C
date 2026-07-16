8314d6ab82e2d32d08e087ff5d71a49efe7a30d1
Sajeda Hmeda <sajedahm18@icloud.com>
2026-07-16 15:44:29 +0200

    Add the c++ code from testing

---
+++ Person.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person{
+
+private:
+   string  name;
+   int  age;
+public:
+    Person();
+   string  getName();
+   int  getAge();
+};
